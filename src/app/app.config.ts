329ed470514fa57269aebb09bd78bfcfbbb2a474
tasnimAlam <tasnimalamcse@gmail.com>
2024-06-10 20:04:20 +0600

    feat: add base store structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 
@@ -7,5 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideStore(),
   ],
 };
