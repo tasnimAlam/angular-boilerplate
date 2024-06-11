48148d0c07c0ae7e8b593590bede76175b6c9862
tasnimAlam <tasnimalamcse@gmail.com>
2024-06-11 14:12:03 +0600

    feat: use modular approach for ui components

---
+++ src/app/shared/ui/index.ts
@@ -0,0 +1,2 @@
+export * from './input/input.module';
+export * from './select/select.module';
--- src/app/shared/ui/select/select/select.component.ts
+++ src/app/shared/ui/select/select/select.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-select',
-  standalone: true,
-  imports: [],
   templateUrl: './select.component.html',
-  styleUrl: './select.component.scss'
+  styleUrl: './select.component.scss',
 })
-export class SelectComponent {
-
-}
+export class SelectComponent {}
