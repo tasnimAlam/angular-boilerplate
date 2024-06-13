668a626ce3b03272706b6bc9074a51ee0fcf3642
tasnimAlam <tasnimalamcse@gmail.com>
2024-06-13 19:27:43 +0600

    feat: add base theme file

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SelectModule, InputModule } from '@ui';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SelectModule, InputModule],
+  imports: [SelectModule, InputModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
