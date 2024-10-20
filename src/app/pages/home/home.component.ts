d3fad91032c398aec53d4551a1b504ab35b5d9d3
tasnimAlam <tasnimalamcse@gmail.com>
2024-10-21 00:04:51 +0600

    chore: update theme config

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { SelectModule, InputModule } from '@ui';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SelectModule, InputModule],
+  imports: [SelectModule, InputModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
